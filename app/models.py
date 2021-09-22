960a6a9dc83244f8f63d959cc9e9dabe8223a058
kevaf <kevinkipyegon20@gmail.com>
2021-09-22 20:25:23 +0300

    modified models

--- app/models.py
+++ app/models.py
@@ -13,6 +13,9 @@ class Complainant(db.Model):
     reports = db.relationship('Report', backref = 'complainant', lazy = "dynamic")
     officers = db.relationship('Officers', backref = 'complainant', lazy = "dynamic")
 
+    def __repr__(self):
+        return f'Complainant {self.fullname}'
+
 class Report(db.Model):
     __tablename__ = "reports"
 
@@ -22,11 +25,35 @@ class Report(db.Model):
     caseDetails = db.Column(db.String(5000))
     complainantId = db.Column(db.Integer, db.ForeignKey("complainants.id"))
     reports = db.relationship('Officers', backref = 'report', lazy = "dynamic")
-   
+    
+    def save_report(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_reports(cls, id):
+        report = Report.query.filter_by(id=id).first()
+
+        return report 
+
 class Officers(db.Model):
     __tablename__ = "officers"
 
     id = db.Column(db.Integer, primary_key = True)
     officername = db.Column(db.String(255))
+    comment = db.Column(db.String(255)) 
     complainantId = db.Column(db.Integer, db.ForeignKey("complainants.id"))
     report = db.Column(db.Integer, db.ForeignKey("reports.id"))
+
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, report):
+        comments= Officers.query.filter_by(report_id=report).all()
+
+        return comments
+
+    
